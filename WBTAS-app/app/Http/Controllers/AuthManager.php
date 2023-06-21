a27141751b998fd1d343ddde42d6b0715b6f7db5
Ivan Jefferson O. Calleja <biboy.calleja0872@gmail.com>
2023-06-21 17:08:42 +0800

    Add Controllers/Modified Views & Routes

---
+++ WBTAS-app/app/Http/Controllers/AuthManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthManager extends Controller
+{
+    function login(){
+    return view('login'); 
+    }
+
+    function registration(){
+    return view('registration'); 
+    }
+
+    function loginPost(Request $request){
+        $request->validate([
+        'email' => 'required',
+        'password' => 'required'
+
+        ])
+    }
+
+}
--- WBTAS-app/resources/views/login.blade.php
+++ WBTAS-app/resources/views/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="container">
-        <form class="ms-auto me-auto" style="width: 500px" >
+        <form action="{{route('"login.post')}}" method="POST"class="ms-auto me-auto" style="width: 500px" >
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email Address</label>
                 <input type="email" class="form-control" name="email">
